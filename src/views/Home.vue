f15aad4581c12879b072f398ad5e77e512efeb40
Kazuki Ota <kaota@microsoft.com>
2021-09-01 13:09:29 +0900

    add demo code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div>{{ message }}</div>
+    <div>
+      <button @click="invokeApi">Invoke API</button>
+    </div>
+    <div>
+      <a href="/login">Login</a>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, ref, computed } from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
+  setup: () => {
+    const name = ref('');
+    const message = computed(() => name.value ? `こんにちは ${name.value} さん` : 'ログインしてボタンを押してください。');
+
+    return {
+      name,
+      message,
+    };
   },
+  methods: {
+    async invokeApi() {
+      try {
+        this.name = await axios.get('/api/Function1?name=sample');
+      } catch (e) {
+        this.name = '';
+        alert('エラー');
+      }
+    }
+  }
 });
 </script>
